881ef30c784159dfa8e79479552c4a4e30cb35b6
Nishanshi <nishanshi.shukla@gmail.com>
2015-04-24 23:05:53 +0530

    lab assignment

---
+++ least_slack_time.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+void main()
+{
+
+ int number,i,waiting,temp1;
+ 	
+ int q = 0;
+ int flag2 = 1;
+ int s = 10000;
+ int j = -1;
+ int cnt = 0;
+ 
+ printf("\nEnter the no of processes:");
+ scanf("%d",&number);
+ 
+ int burst[number],begin[number],end[number], flag[number];
+ for(i=0;i<number;i++)
+	flag[number] = 0;
+ 
+ waiting = 0;
+ 
+ for(i=0;i<number;i++)
+ {
+ 	printf("\nEnter absolute begin time, burst time and absolute end for process %d:",i+1);
+ 	scanf("%d %d %d", &begin[i], &burst[i], &end[i]);
+ }
+
+ while(flag2)
+ {
+	for(i = 0; i< number;i++)
+	{
+	
+		if(begin[i] == q || burst[j] == 0)
+		{
+			
+			
+			if(flag[i] != 1)
+			{
+				if(((end[i] - q) - burst[i]) < s)
+				{
+					sm = (end[i] - q);
+					j = i;
+				}
+			}
+		}
+		
+		
+	}
+	if(burst[j] == 0)
+			{
+				s = 10000;
+				j=-1;
+			}
+	if(j!=-1)
+	{
+	printf("\n From time quantum %d to %d, proc %d", q,q+1, j+1);
+	burst[j]--;
+	if(burst[j] == 0)
+	{
+		flag[j]=1;
+		cnt++;
+	}
+	}
+	if(cnt == number)
+		flag2 = 0;
+	
+ q++;
+ }
+ 
+}
